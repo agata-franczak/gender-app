3ecff2af3416b505f5c2f38cfc09d60c395f0c0a
agata-franczak <agata@timberseed.com>
2026-02-27 16:05:15 +0100

    Configure app to run on specified port and debug mode

--- app.py
+++ app.py
@@ -33,4 +33,6 @@ def upload():
     return send_file(unique_name, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
